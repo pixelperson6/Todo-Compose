25ae781c4f653a2aa1873c146653f89c137f5e05
Shiv sah <pixelperson6@gmail.com>
2022-02-06 06:31:45 +0545

    delete task added

--- app/src/main/java/com/codingwithpix3l/todo_compose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/codingwithpix3l/todo_compose/ui/viewmodels/SharedViewModel.kt
@@ -51,11 +51,23 @@ class SharedViewModel @Inject constructor(private val repository: TodoRepository
         }
     }
 
+    private fun deleteTask(){
+        viewModelScope.launch(Dispatchers.IO) {
+            val todoTask = TodoTask(
+                id=id.value,
+                title = title.value,
+                disc = description.value,
+                priority = priority.value
+            )
+            repository.deleteTask(todoTask = todoTask)
+        }
+    }
+
     fun handleDatabaseAction(action: Action){
         when(action){
             Action.ADD -> addTask()
             Action.UPDATE -> updateTask()
-            Action.DELETE -> {}
+            Action.DELETE -> deleteTask()
             Action.DELETE_ALL -> {}
             Action.UNDO -> {}
             else->{
